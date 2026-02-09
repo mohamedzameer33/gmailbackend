8ecca9284f2b62c6fd63e9c26f8e918e10ed2ef6
Mohamed Zameer S M <132653628+mohamedzameer33@users.noreply.github.com>
2026-02-09 23:00:24 +0530

    Update SendGridConfig.java

--- src/main/java/com/example/gmail/Configuration/SendGridConfig.java
+++ src/main/java/com/example/gmail/Configuration/SendGridConfig.java
@@ -4,6 +4,15 @@ import com.sendgrid.SendGrid;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.sendgrid.Attachments;
+import com.sendgrid.Content;
+import com.sendgrid.Email;
+import com.sendgrid.Mail;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+
 
 @Configuration
 public class SendGridConfig {
