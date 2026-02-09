43e4099479323ffca2e6fd0f47dcba8b59db3338
Mohamed Zameer S M <132653628+mohamedzameer33@users.noreply.github.com>
2026-02-09 20:55:03 +0530

    Update EmailController.java

--- src/main/java/com/example/gmail/Controller/EmailController.java
+++ src/main/java/com/example/gmail/Controller/EmailController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/mail")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://zam-gmailtemp.vercel.app")
 public class EmailController {
 
     @Autowired private EmailService emailService;
